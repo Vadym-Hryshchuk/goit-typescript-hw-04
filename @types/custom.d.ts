330d2a8c8bf0b1fbe56cf302a254236ff3cf545a
Vadym-Hryshchuk <82964921+Vadym-Hryshchuk@users.noreply.github.com>
2023-10-22 22:54:11 +0300

    add homework

---
+++ @types/custom.d.ts
@@ -0,0 +1,12 @@
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: any;
+  export default content;
+}
+declare module "react/jsx-runtime" {
+    export default any;
+  }
